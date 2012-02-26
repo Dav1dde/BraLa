7e95fb9ddb18cb1918b3f3ac9553adc4d1f5d716
David Herberth <admin@dav1d.de>
2012-02-26 17:42:02 +0100

    removed packets.d

--- brala/network/packets.d
+++
@@ -1,5 +0,0 @@
-module brala.network.packets;
-
-
-private {
-}
--- brala/network/protocol.d
+++ brala/network/protocol.d
@@ -5,6 +5,7 @@ private {
 }
 
 
+// Mixin which sends the client packets, there must be a send function/template provided.
 mixin template ClientProtocol() {
     
 }
