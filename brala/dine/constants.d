2b292c537f937207ba83f1cba0fa92fce423b214
David Herberth <admin@dav1d.de>
2012-03-07 17:20:39 +0100

    added constants

---
+++ brala/dine/constants.d
@@ -0,0 +1,48 @@
+module brala.dine.constants;
+
+
+enum Biome {
+    Ocean = 0,
+    Plains,
+    Desert,
+    ExtremeHills,
+    Forest,
+    Taiga,
+    Swampland,
+    River,
+    Hell,
+    Sky,
+    FrozenOcean,
+    FrozenRiver,
+    IcePlains,
+    IceMountains,
+    MushroomIsland,
+    MushroomIslandShore,
+    Beach,
+    DesertHills,
+    ForestHills,
+    TaigaHills,
+    ExtremeHillsEdge,
+    Jungle,
+    JungleHills
+}
+
+enum Coal {
+    Coal = 0,
+    CharCoal
+}
+
+enum Jukebox {
+    Nothing = 0,
+    GoldDisc,
+    GreenDisc,
+    OrangeDisc,
+    RedDisc,
+    LimeDisc,
+    PurpleDisc,
+    VioletDisc,
+    BlackDisc,
+    WhiteDisc,
+    SeaGreenDisc,
+    BrokenDisc
+}
