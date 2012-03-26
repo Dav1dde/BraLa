ece1ff17ab2ff835b8c41094c43bc759c5425cb8
David Herberth <admin@dav1d.de>
2012-03-26 16:26:49 +0200

    moved tessellation from chunk to world

--- brala/dine/chunk.d
+++ brala/dine/chunk.d
@@ -1,19 +1,13 @@
 module brala.dine.chunk;
 
 private {
-    import glamour.gl : GLuint, GLenum, GL_FLOAT;
+    import glamour.gl : GLuint, GLenum;
     import glamour.vbo : Buffer;
     
-    import std.c.string : memcpy;
     import std.bitmanip : bitfields;
     
     import gl3n.linalg : vec3i;
-
-    import brala.dine.vertices : BLOCK_VERTICES_LEFT, BLOCK_VERTICES_RIGHT, BLOCK_VERTICES_NEAR,
-                                 BLOCK_VERTICES_FAR, BLOCK_VERTICES_TOP, BLOCK_VERTICES_BOTTOM;
-    import brala.dine.util : calloc, malloc, realloc, free, log2_ub;
-    import brala.engine : BraLaEngine;
-    import brala.utils.ctfe : TupleRange;
+    import brala.dine.util : calloc, free, log2_ub;
 }
 
 
@@ -34,17 +28,6 @@ struct Block {
     }
 }
 
-private const Block AIR_BLOCK = Block(0);
-
-private struct NeighbourChunks {
-    Chunk* left;
-    Chunk* right;
-    Chunk* near;
-    Chunk* far;
-    Chunk* top;
-    Chunk* bottom;
-}
-
 // NOTE to prgrammer, ctor will maybe called from a seperate thread
 // => dont do opengl stuff in the ctor
 class Chunk {
@@ -52,7 +35,8 @@ class Chunk {
     const int width = 16;
     const int height = 256;
     const int depth = 16;
-    
+
+    const int zstep = width*height;
     const int log2width = log2_ub(width);
     const int log2height = log2_ub(height);
     const int log2depth = log2_ub(depth);
@@ -73,8 +57,6 @@ class Chunk {
     
     bool dirty;
 
-    NeighbourChunks neighbour_chunks;
-    
     bool empty;
     Block* blocks;
     ubyte[] biome_data;
@@ -143,219 +125,13 @@ class Chunk {
     }
     
     Block get_block_safe(int x, int y, int z) {
-        if(x < width && y < height && z < depth) {
+        if(x >= 0 && x < width && y >= 0 && y < height && z >= 0 && z < depth) {
             return get_block(to_flat(x, y, z));
         } else {
             return Block(0, 0);
         }
     }
-
-    Block blockss(int i) {
-        if(i < 0) return Block(0, 0);
-        if(i > block_count) return Block(0, 0);
-        return blocks[i];
-    }
-    
-    // rendering
-    
-    // fills the vbo with the chunk content    
-    // credits to Florian Boesch - http://codeflow.org/
-    void tessellate(ref float* v, ref size_t length, bool force = false){
-        if(vbo is null) {
-            vbo = new Buffer();
-        }
-
-        if(dirty || force) {
-            int zstep = width*height;
-            int index;
-            int w = 0;
-
-            float x_offset;
-            float x_offset_r;
-            float y_offset;
-            float y_offset_t;
-            float z_offset;
-            float z_offset_n;
-
-            Block value;
-            Block right_block;
-            Block top_block;
-            Block front_block;
-
-            foreach(z; -1..depth) {
-                z_offset = z+0.5f;
-                z_offset_n = z+1.5f;
-
-                foreach(y; -1..height) {
-                    y_offset = y+0.5f;
-                    y_offset_t = y+1.5f;
-
-                    if(z == -1 || z == depth-1 || y == -1 || y == height-1 || neighbour_chunks.left is null) {
-                        value = AIR_BLOCK;
-                    } else {
-                        value = neighbour_chunks.left.blocks[(width-1)+y*width+z*zstep];
-                    }
-
-                    if(w+1024 > length) {
-                        length = length + (depth-z)*width*height;
-                        v = cast(float*)realloc(v, length*float.sizeof);
-                    }
-
-                    foreach(x; -1..width) {
-                        x_offset = x+0.5f;
-                        x_offset_r = x+1.5f;
-
-                        index = x+y*width+z*zstep;
-
-                        if(z == -1) {
-                            right_block = AIR_BLOCK;
-                            top_block = AIR_BLOCK;
-
-                            if(y >= 0 && y < height-1 && x >= 0) {
-                                front_block = blocks[index+zstep];
-                            } else {
-                                front_block = AIR_BLOCK;
-                            }
-                        } else if(z == depth-1) {
-                            if(x < width-1) {
-                                right_block = blocks[index+1];
-                            } else {
-                                right_block = AIR_BLOCK;
-                            }
-
-                            if(x >= 0 && y < height-1) {
-                                top_block = blocks[index+width];
-                            } else {
-                                top_block = AIR_BLOCK;
-                            }
-
-                            front_block = AIR_BLOCK;
-                        } else {
-                            if(y == height-1) {
-                                right_block = AIR_BLOCK;
-                                top_block = AIR_BLOCK;
-                                front_block = AIR_BLOCK;
-                            } else if(y == -1) {
-                                right_block = AIR_BLOCK;
-                                front_block = AIR_BLOCK;
-
-                                if(x >= 0 || x < width-1 || z >= 0 || z < depth-1) {
-                                    top_block = blocks[index+width];
-                                } else {
-                                    top_block = AIR_BLOCK;
-                                }
-                            } else {
-                                if(x == -1) {
-                                    right_block = blocks[index+1];
-                                    top_block = AIR_BLOCK;
-                                    front_block = AIR_BLOCK;
-                                } else if(x == width-1) {
-                                    right_block = AIR_BLOCK;
-                                    top_block = blocks[index+width];
-                                    front_block = blocks[index+zstep];
-                                } else {
-                                    right_block = blocks[index+1];
-                                    top_block = blocks[index+width];
-                                    front_block = blocks[index+zstep];
-                                }
-                            }
-                        }
-                                                
-                        // TODO: use primary bitmask
-                        // TODO: octree?
-                        // TODO: maybe dont use a "template vertices" for easy blocks, which only consist of 2 triangles per side
-                        if(value == 0) {
-                            if(right_block.id != 0) {
-                                float[] vertices = BLOCK_VERTICES_LEFT[right_block.id];
-                                v[w..(w+(vertices.length))] = vertices;
-                                size_t rw = w;
-                                for(; w < (rw+vertices.length); w += 5) {
-                                    v[w++] += x_offset_r;
-                                    v[w++] += y_offset;
-                                    v[w++] += z_offset;
-                                }
-                            }
-                            if(top_block.id != 0) {
-                                float[] vertices = BLOCK_VERTICES_BOTTOM[top_block.id];
-                                v[w..(w+(vertices.length))] = vertices;
-                                size_t rw = w;
-                                for(; w < (rw+vertices.length); w += 5) {
-                                    v[w++] += x_offset;
-                                    v[w++] += y_offset_t;
-                                    v[w++] += z_offset;
-                                }
-                            }
-                            if(front_block.id != 0) {
-                                float[] vertices = BLOCK_VERTICES_FAR[front_block.id];
-                                v[w..(w+(vertices.length))] = vertices;
-                                size_t rw = w;
-                                for(; w < (rw+vertices.length); w += 5) {
-                                    v[w++] += x_offset;
-                                    v[w++] += y_offset;
-                                    v[w++] += z_offset_n;
-                                }
-                            }
-                        } else if(x >= 0) {
-                            if(right_block == 0) {
-                                float[] vertices = BLOCK_VERTICES_RIGHT[value.id];
-                                v[w..(w+(vertices.length))] = vertices;
-                                size_t rw = w;
-                                for(; w < (rw+vertices.length); w += 5) {
-                                    v[w++] += x_offset;
-                                    v[w++] += y_offset;
-                                    v[w++] += z_offset;
-                                }
-                            }
-                            if(top_block == 0) {
-                                float[] vertices = BLOCK_VERTICES_TOP[value.id];
-                                v[w..(w+(vertices.length))] = vertices;
-                                size_t rw = w;
-                                for(; w < (rw+vertices.length); w += 5) {
-                                    v[w++] += x_offset;
-                                    v[w++] += y_offset;
-                                    v[w++] += z_offset;
-                                }
-                            }
-                            if(front_block == 0) {
-                                float[] vertices = BLOCK_VERTICES_NEAR[value.id];
-                                v[w..(w+(vertices.length))] = vertices;
-                                size_t rw = w;
-                                for(; w < (rw+vertices.length); w += 5) {
-                                    v[w++] += x_offset;
-                                    v[w++] += y_offset;
-                                    v[w++] += z_offset;
-                                }
-                            }
-                        }
-
-                        value = right_block;
-                    }
-                }
-            }
-
-            vbo_type = GL_FLOAT;
-            vbo_vcount = w / 8; // 8 = vertex: x,y,z, normal: xn, yn, zn, texcoords: u, v
-            vbo.set_data(v[0..w], GL_FLOAT);
-
-            dirty = false;
-        }
-    }
-    
-    void bind(BraLaEngine engine)
-        in { assert(vbo !is null); assert(engine.current_shader !is null, "no current shader"); }
-        body {
-            GLuint position = engine.current_shader.get_attrib_location("position");
-            GLuint normal = engine.current_shader.get_attrib_location("normal");
-            GLuint texcoord = engine.current_shader.get_attrib_location("texcoord");
-            
-            // stride = vertex: x,y,z, normal: xn, xy, xz, texcoords: u, v
-            uint stride = (3+3+2)*float.sizeof;
-            
-            vbo.bind(position, 3, 0, stride);
-            vbo.bind(normal, 3, 3*float.sizeof, stride);
-            vbo.bind(texcoord, 2, (3+3)*float.sizeof, stride);
-        }
-    
+        
     // operator overloading
     Block opIndex(size_t flat)
     in { assert(!empty); assert(flat < block_count); }
@@ -410,18 +186,18 @@ class Chunk {
         }
          
     // static stuff
-    static uint to_flat(vec3i inp) {
+    static int to_flat(vec3i inp) {
         return to_flat(inp.x, inp.y, inp.z);
     }
     
-    static uint to_flat(uint x, uint y, uint z)
-        in { assert(x < width && y < height && z < depth); }
+    static int to_flat(int x, int y, int z)
+        in { assert(x >= 0 && x < width && y >= 0 && y < height && z >= 0 && z < depth); }
         out (result) { assert(result < block_count); }
         body {
-            return x + y*width + z*width*height;
+            return x + y*width + z*zstep;
         }
     
-    static vec3i from_flat(uint flat)
+    static vec3i from_flat(int flat)
         in { assert(flat < block_count); }
         out (result) { assert(result.vector[0] < width && result.vector[1] < height && result.vector[2] < depth); }
         body {
--- brala/dine/world.d
+++ brala/dine/world.d
@@ -1,16 +1,20 @@
 module brala.dine.world;
 
 private {
-    import glamour.gl : glDrawArrays, GL_TRIANGLES;
+    import glamour.gl : GLuint, glDrawArrays, GL_TRIANGLES, GL_FLOAT;
+    import glamour.vbo : Buffer;
     
     import gl3n.linalg : vec3i, mat4;
     
     import brala.dine.chunk : Chunk, Block;
-    import brala.dine.util : malloc, free;
+    import brala.dine.vertices : BLOCK_VERTICES_LEFT, BLOCK_VERTICES_RIGHT, BLOCK_VERTICES_NEAR,
+                                 BLOCK_VERTICES_FAR, BLOCK_VERTICES_TOP, BLOCK_VERTICES_BOTTOM;
+    import brala.dine.util : malloc, realloc, free
     import brala.exception : WorldError;
     import brala.engine : BraLaEngine;
 }
 
+private const Block AIR_BLOCK = Block(0);
 
 class World {
     static float* tessellate_buffer;
@@ -27,6 +31,7 @@ class World {
     const int width = 16;
     const int height = 256;
     const int depth = 16;
+    const int zstep = width*height;
     const int min_height = 0;
     const int max_height = height;    
     
@@ -54,7 +59,6 @@ class World {
         
         chunks[chunkc] = chunk;
         mark_surrounding_chunks_dirty(chunkc);
-        update_neighbours(&chunk, chunkc);
     }
     
     void remove_chunk(vec3i chunkc)
@@ -64,7 +68,6 @@ class World {
             chunks.remove(chunkc);
 
             mark_surrounding_chunks_dirty(chunkc);
-            update_neighbours(chunkc);
         }
     
     void remove_all_chunks() {
@@ -79,7 +82,10 @@ class World {
     }
     
     Chunk get_chunk(vec3i chunkc) {
-        return chunks[chunkc];
+        if(Chunk* c = chunkc in chunks) {
+            return *c;
+        }
+        return null;
     }
     
     void set_block(vec3i position, Block block)
@@ -113,33 +119,19 @@ class World {
             return chunk[chunk.to_flat(position.x % width, position.y % height, position.z % depth)];
         }
 
-    void update_neighbours(vec3i chunkc) {
-        Chunk* c = chunkc in chunks;
-        update_neighbours(c, chunkc);
-    }
-        
-    void update_neighbours(Chunk* chunk, vec3i chunkc) {
-        Chunk* c_left = vec3i(chunkc.x-1, chunkc.yz) in chunks;
-        Chunk* c_right = vec3i(chunkc.x+1, chunkc.yz) in chunks;
-        Chunk* c_near = vec3i(chunkc.xy, chunkc.z+1) in chunks;
-        Chunk* c_far = vec3i(chunkc.xy, chunkc.z-1) in chunks;
-        Chunk* c_top = vec3i(chunkc.x, chunkc.y+1, chunkc.z) in chunks;
-        Chunk* c_bottom = vec3i(chunkc.x, chunkc.y-1, chunkc.z) in chunks;
+    Block get_block_safe(vec3i position, Block def = AIR_BLOCK) {
+        Chunk chunk = get_chunk(position.x / width, position.y / height, position.z / depth);
 
-        if(chunk is null) {
-            if(c_left !is null)     c_left.neighbour_chunks.right = chunk;
-            if(c_right !is null)   c_right.neighbour_chunks.left = chunk;
-            if(c_near !is null)     c_near.neighbour_chunks.far = chunk;
-            if(c_far !is null)       c_far.neighbour_chunks.near = chunk;
-            if(c_top !is null)       c_top.neighbour_chunks.bottom = chunk;
-            if(c_bottom !is null) c_bottom.neighbour_chunks.top = chunk;
+        if(chunk is null) return def;
+
+        int x = position.x % width;
+        int y = position.y % height;
+        int z = position.z % depth;
+
+        if(x >= 0 && x < chunk.width && y >= 0 && y < chunk.height && z >= 0 && z < chunk.depth) {        
+            return chunk[chunk.to_flat(x, y, z)];
         } else {
-            chunk.neighbour_chunks.left = c_left;
-            chunk.neighbour_chunks.right = c_right;
-            chunk.neighbour_chunks.near = c_near;
-            chunk.neighbour_chunks.far = c_far;
-            chunk.neighbour_chunks.top = c_top;
-            chunk.neighbour_chunks.bottom = c_bottom;
+            return def;
         }
     }
 
@@ -165,11 +157,143 @@ class World {
             c.dirty = true;
         }
     }
+   
+    bool is_air(Chunk chunk, vec3i chunkc, int x, int y, int z) {
+        if(x >= 0 && x < width && y >= 0 && y < height && z >= 0 && z < depth) {
+            return chunk.blocks[x+y*width+z*zstep].id == 0;
+        } else {
+            return get_block_safe(vec3i(chunkc.x*chunk.width+x, chunkc.y*chunk.height+y, chunkc.z*chunk.depth+z), AIR_BLOCK).id == 0;
+        }
+    }
+    
+    // rendering
+    void tessellate(Chunk chunk, vec3i chunkc,
+                    ref float* v = tessellate_buffer, ref size_t length = tessellate_buffer_length,
+                    bool force = false) {
+        if(chunk.vbo is null) {
+            chunk.vbo = new Buffer();
+        }
+
+        if(chunk.dirty || force) {
+            int index;
+            int w = 0;
+
+            float z_offset;
+            float y_offset;
+            float x_offset;
+            
+            Block value;
+
+            // TODO: use the optimized code with 3 lookups
+            // TODO: primary bitmask, octree?
+
+            foreach(z; 0..depth) {
+                z_offset = z+0.5f;
+                foreach(y; 0..height) {
+                    y_offset = y+0.5f;
+
+                    if(w+1024 > length) {
+                        length = length + (depth-z)*width*height;
+                        v = cast(float*)realloc(v, length*float.sizeof);
+                    }
+
+                    foreach(x; 0..width) {
+                        x_offset = x+0.5f;
+                        
+                        value = chunk.blocks[x+y*width+z*zstep];
+
+                        if(is_air(chunk, chunkc, x-1, y, z)) {
+                            float[] vertices = BLOCK_VERTICES_LEFT[value.id];
+                            v[w..(w+(vertices.length))] = vertices;
+                            size_t rw = w;
+                            for(; w < (rw+vertices.length); w += 5) {
+                                v[w++] += x_offset;
+                                v[w++] += y_offset;
+                                v[w++] += z_offset;
+                            }
+                        }
+                        if(is_air(chunk, chunkc, x, y-1, z)) {
+                            float[] vertices = BLOCK_VERTICES_BOTTOM[value.id];
+                            v[w..(w+(vertices.length))] = vertices;
+                            size_t rw = w;
+                            for(; w < (rw+vertices.length); w += 5) {
+                                v[w++] += x_offset;
+                                v[w++] += y_offset;
+                                v[w++] += z_offset;
+                            }
+                        }
+                        if(is_air(chunk, chunkc, x, y, z-1)) {
+                            float[] vertices = BLOCK_VERTICES_FAR[value.id];
+                            v[w..(w+(vertices.length))] = vertices;
+                            size_t rw = w;
+                            for(; w < (rw+vertices.length); w += 5) {
+                                v[w++] += x_offset;
+                                v[w++] += y_offset;
+                                v[w++] += z_offset;
+                            }
+                        }
+                        if(is_air(chunk, chunkc, x+1, y, z)) {
+                            float[] vertices = BLOCK_VERTICES_RIGHT[value.id];
+                            v[w..(w+(vertices.length))] = vertices;
+                            size_t rw = w;
+                            for(; w < (rw+vertices.length); w += 5) {
+                                v[w++] += x_offset;
+                                v[w++] += y_offset;
+                                v[w++] += z_offset;
+                            }
+                        }
+                        if(is_air(chunk, chunkc, x, y+1, z)) {
+                            float[] vertices = BLOCK_VERTICES_TOP[value.id];
+                            v[w..(w+(vertices.length))] = vertices;
+                            size_t rw = w;
+                            for(; w < (rw+vertices.length); w += 5) {
+                                v[w++] += x_offset;
+                                v[w++] += y_offset;
+                                v[w++] += z_offset;
+                            }
+                        }
+                        if(is_air(chunk, chunkc, x, y, z+1)) {
+                            float[] vertices = BLOCK_VERTICES_NEAR[value.id];
+                            v[w..(w+(vertices.length))] = vertices;
+                            size_t rw = w;
+                            for(; w < (rw+vertices.length); w += 5) {
+                                v[w++] += x_offset;
+                                v[w++] += y_offset;
+                                v[w++] += z_offset;
+                            }
+                        }
+                    }
+                }
+            }
+
+            chunk.vbo_type = GL_FLOAT;
+            chunk.vbo_vcount = w / 8; // 8 = vertex: x,y,z, normal: xn, yn, zn, texcoords: u, v
+            chunk.vbo.set_data(v[0..w], GL_FLOAT);
+
+            chunk.dirty = false;
+        }
+    }
+
+    void bind(BraLaEngine engine, Chunk chunk)
+        in { assert(chunk.vbo !is null); assert(engine.current_shader !is null, "no current shader"); }
+        body {
+            GLuint position = engine.current_shader.get_attrib_location("position");
+            GLuint normal = engine.current_shader.get_attrib_location("normal");
+            GLuint texcoord = engine.current_shader.get_attrib_location("texcoord");
+
+            // stride = vertex: x,y,z, normal: xn, xy, xz, texcoords: u, v
+            uint stride = (3+3+2)*float.sizeof;
+
+            chunk.vbo.bind(position, 3, 0, stride);
+            chunk.vbo.bind(normal, 3, 3*float.sizeof, stride);
+            chunk.vbo.bind(texcoord, 2, (3+3)*float.sizeof, stride);
+        }
+
     
     void draw(BraLaEngine engine) {
         foreach(chunkc, chunk; chunks) {
-            chunk.tessellate(tessellate_buffer, tessellate_buffer_length, false);
-            chunk.bind(engine);
+            tessellate(chunk, chunkc, tessellate_buffer, tessellate_buffer_length, false);
+            bind(engine, chunk);
             
             engine.model = mat4.translation(chunkc.x*width, chunkc.y*height, chunkc.z*depth);
             engine.flush_uniforms();
