6018e56fcfb8605262cbd580d4c2e79bfde36bdd
David Herberth <admin@dav1d.de>
2012-04-05 18:18:52 +0200

    meh, forgot to add the two "new" files

---
+++ brala/utils/defaultaa.d
@@ -0,0 +1,63 @@
+module brala.utils.defaultaa;
+
+private import std.traits : ReturnType, isCallable;
+
+struct DefaultAA(value_type, key_type, alias default__) {
+    private value_type[key_type] _store;
+    alias _store this;
+    alias default__ default_;
+
+    static if(isCallable!default_) {
+        static assert(is(ReturnType!(default_) : value_type), "callable returntype doesn't match value_type");
+    }
+
+    private value_type _get_default() {
+        static if(isCallable!default_) {
+            return default_();
+        } else {
+            return default_;
+        }
+    }
+
+    value_type opIndex(key_type key) {
+        if(key !in _store) {
+            _store[key] = _get_default();
+        }
+
+        return _store[key];
+    }
+
+    void opIndexAssign(value_type value, key_type key) {
+        _store[key] = value;
+    }
+
+    void opIndexOpAssign(string op)(value_type rhs, key_type key) {
+        if(key !in _store) {
+            _store[key] = _get_default();
+        }
+
+        mixin("_store[key]" ~ op ~"= rhs;");
+    }
+}
+
+unittest {
+    DefaultAA!(int, string, 12) myaa;
+    assert(myaa["baz"] == 12);
+    assert(myaa["foo"] == 12);
+    myaa["baz"] = -12;
+    assert(myaa["baz"] == -12);
+    assert(myaa["foo"] == 12);
+    myaa["baz"] += 12;
+    assert(myaa["baz"] == 0);
+    myaa["foo"] -= 12;
+    assert(myaa["foo"] == 0);
+    myaa["lulz"] -= 12;
+    assert(myaa["lulz"] == 0);
+
+    int dg() { return 1; }
+
+    DefaultAA!(int, string, dg) mydgaa;
+    assert(mydgaa["foo"] == 1);
+    mydgaa["lulz"] -= 1;
+    assert(mydgaa["lulz"] == 0);
+}
---
+++ brala/utils/image.d
@@ -0,0 +1,103 @@
+module brala.utils.image;
+
+private {
+    import stb_image : stbi_load, stbi_image_free;
+    import glamour.gl : GL_RGB, GL_RGBA, GL_UNSIGNED_BYTE;
+    import glamour.texture : Texture2D;
+
+    import std.string : toStringz, format;
+
+    import brala.exception : ImageError;
+}
+
+class Image {
+    ubyte[] data;
+    int width;
+    int height;
+    int comp;
+
+    @property int dest_format() {
+        if(comp == 3) {
+            return GL_RGB;
+        } else if(comp == 4) {
+            return GL_RGBA;
+        } else {
+            throw new ImageError("Unknown/Unsupported stbi image format");
+        }
+    }
+    
+    const int dest_type = GL_UNSIGNED_BYTE;
+
+    this(ubyte[] data, int width, int height, int comp) {
+        this.data = data;
+        this.width = width;
+        this.height = height;
+        this.comp = comp;
+    }
+
+    static Image from_file(string filename) {
+        int x;
+        int y;
+        int comp;
+        ubyte* data = stbi_load(toStringz(filename), &x, &y, &comp, 0);
+
+        if(data is null) {
+            throw new ImageError("Unable to load image: " ~ filename);
+        }
+
+        scope(exit) stbi_image_free(data);
+
+        if(!(comp == 3 || comp == 4)) {
+            throw new ImageError("Unknown/Unsupported stbi image format");
+        }
+        
+        return new Image(data[0..x*y*comp].dup, x, y, comp);
+    }
+
+    ubyte[] get_pixel(int x, int y)
+        in { assert(x < width && y < height); }
+        body {
+            return data[x*comp+y*width*comp..comp+x*comp+y*width*comp];
+        }
+
+    Image crop(int x1, int y1, int x2, int y2)
+        in { assert(x1 < x2 && x2 < width && y1 < y2 && y2 < height); }
+        body {
+            ubyte[] res;
+
+            foreach(y; y1..y2) {
+                res ~= data[x1*comp+y*width*comp..x2*comp+y*width*comp];
+            }
+
+            return new Image(res, x2-x1, y2-y1, comp);
+        }
+
+    void replace(int x, int y, Image img)
+        in { assert(img.width+x <= width && img.height+y <= height && comp == img.comp); }
+        body {
+            foreach(yc; 0..img.height) {
+                int yindex_bigimg = (y+yc)*width*comp;
+                int yindex_smallimg = yc*(img.width)*comp;
+                
+                data[x*comp+yindex_bigimg..(x+img.width)*comp+yindex_bigimg] =
+                    img.data[yindex_smallimg..(img.width)*comp+yindex_smallimg];
+            }
+        }
+
+    void resize(int new_width, int new_height) {
+        ubyte[] new_data = new ubyte[new_width*new_height*comp];
+
+        new_data[0..width*height*comp] = data;
+
+        this.data = data;
+        this.width = new_width;
+        this.height = new_height;
+    }
+
+    Texture2D to_texture() {
+        Texture2D ret = new Texture2D(dest_format, width, height, dest_format, dest_type);
+        ret.set_data(data);
+        return ret;
+    }
+        
+}
