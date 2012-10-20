3450cd13fab11b27be8db34c4f1d077217078704
David <admin@dav1d.de>
2012-10-20 16:56:17 +0200

    added stdout hook, which does nothing

--- brala/utils/stdio.d
+++ brala/utils/stdio.d
@@ -15,6 +15,8 @@ private void stderr_hook(File f, bool pre) {
     }
 }
 
+private void stdout_hook(File f, bool pre) {}
+
 private struct HookedFD(alias hook) {
     File _fd;
     alias _fd this;
@@ -26,8 +28,28 @@ private struct HookedFD(alias hook) {
     }
 }
 
+__gshared HookedFD!(stdout_hook) stdout;
 __gshared HookedFD!(stderr_hook) stderr;
 
 shared static this() {
+    stdout._fd = std.stdio.stdout;
     stderr._fd = std.stdio.stderr;
 }
+
+private string inject(string hook_name) {
+    string res;
+
+    foreach(m; __traits(allMembers, std.stdio)) {
+        static if(m.length >= 5 && m[0..5] == "write") {
+            res ~= `void ` ~ m ~ `(Args...)(auto ref Args args) {
+                        ` ~ hook_name ~ `(std.stdio.stdout, true);
+                        std.stdio.stdout.` ~ m ~ `(args);
+                        ` ~ hook_name ~ `(std.stdio.stdout, false);
+                    }`;
+        }
+    }
+
+    return res;
+}
+
+mixin(inject("stdout_hook"));
