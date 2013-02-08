4102e1f2e7dab6650a31f229852a57242d741ea0
David <admin@dav1d.de>
2013-02-08 19:31:43 +0100

    fixed is_debugged

--- brala/log.d
+++ brala/log.d
@@ -3,6 +3,8 @@ module brala.log;
 private {
     import std.path : dirName, absolutePath, buildPath;
     import core.runtime : Runtime;
+
+    import brala.utils.debugger : is_debugged;
 }
 
 public import brala.utils.log;
@@ -34,6 +36,10 @@ __gshared Logger api_logger;
 __gshared Logger session_logger;
 
 shared static this() {
+    // on linux this function relies on open file descriptors
+    // so call it before we are opening FDs
+    is_debugged();
+    
     main_logger = new BraLaLogger("Main");
     memory_logger = new BraLaLogger("Memory");
     resmgr_logger = new BraLaLogger("Resmgr");
--- brala/utils/debugger.d
+++ brala/utils/debugger.d
@@ -6,29 +6,37 @@ private {
     }
 }
 
-private __gshared bool _is_debugged = false;
-
-@property bool is_debugged() {
-    return _is_debugged;
-}
-
 version(Posix) {
-    shared static this() {
-        // detects gdb:
-        // gdb opens file descriptors 3, 4 and 5 and doesn't close them
-        // so if we have a fd > 5 on startup we are debugged
-        FILE *fd = fopen("/tmp", "r");
+    private __gshared bool _is_debugged = false;
 
-        if (fileno(fd) > 5) {
-            _is_debugged = true;
+    @property bool is_debugged() {
+        static bool init = false;
+
+        if(!init) {
+            // detects gdb:
+            // gdb opens file descriptors 3, 4 and 5 and doesn't close them
+            // so if we have a fd > 5 on startup we are debugged
+
+            FILE *fd = fopen("/tmp", "r");
+            if (fileno(fd) > 5) {
+                _is_debugged = true;
+            }
+            fclose(fd);
+            
+            init = true;
         }
-        
-        fclose(fd);
+
+        return _is_debugged;
+    }
+
+    shared static this() {
+        // call it as soon as possible
+        is_debugged();
     }
 } else version(Windows) {
     extern(System) bool IsDebuggerPresent();
 
-    shared static this() {
-        _is_debugged = IsDebuggerPresent();
+    @property bool is_debugged() {
+        return IsDebuggerPresent();
     }
 }
